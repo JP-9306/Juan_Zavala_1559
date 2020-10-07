9019f729aa1b4881360330fb9922636f013fc392
JP-9306 <rubberhuman1768@gmail.com>
2020-10-06 21:27:32 -0500

    un programa de pilas en python

---
+++ pila.py
@@ -0,0 +1,16 @@
+pila=[3,4,5] #pila de inicio con un arreglo
+print (pila) #impresion de la pila
+#345
+
+pila.append(7)
+pila.append(8)
+
+print (pila)
+
+#34578
+
+print ("saco este elemento", pila.pop())
+#8
+
+print ("queda así la pila:", pila)
+#3457
